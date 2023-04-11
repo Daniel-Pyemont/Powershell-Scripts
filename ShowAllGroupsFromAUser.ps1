a4ffc3d227fc4f2be38cfdf2aad42f1e2c82adc4
Daniel Pyemont <danp0@hotmail.com>
2023-04-12 06:10:56 +1000

    Add files via upload

---
+++ ShowAllGroupsFromAUser.ps1
@@ -0,0 +1,3 @@
+﻿# This will grab all of the groups from a User AD Account.
+# Change the DistinguishedName to the users login name then run script.
+Get-ADPrincipalGroupMembership DistinguishedName | select Name
