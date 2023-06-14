6269e00157513f19ae83aec56a5cb5ecfc1375a9
Daniel Pyemont <2707657+Daniel-Pyemont@users.noreply.github.com>
2023-06-15 07:54:07 +1000

    Add files via upload
    Get a list of users in a security group.

---
+++ GetUsersInSecurityGroup.ps1
@@ -0,0 +1,24 @@
+﻿$groupName = "G-SYDN-RES-FS-11840-MO"
+$outputFile = "C:\Temp\GetUsersinSecurityGroup\Balmain-PERFBUS.csv"
+
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Retrieve the security group members
+$groupMembers = Get-ADGroupMember -Identity $groupName | Where-Object {$_.objectClass -eq 'user'}
+
+# Create an array to store the results
+$results = @()
+
+# Iterate through the group members and add them to the results array
+foreach ($member in $groupMembers) {
+    $user = Get-ADUser -Identity $member.SamAccountName -Properties DisplayName, SamAccountName
+    $result = [PSCustomObject]@{
+        DisplayName    = $user.DisplayName
+        StafflinkID = $user.SamAccountName
+    }
+    $results += $result
+}
+
+# Export the results to a CSV file
+$results | Export-Csv -Path $outputFile -NoTypeInformation
